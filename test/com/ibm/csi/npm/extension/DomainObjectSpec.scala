62786660d2c1ab8dce43d8bc78ba778624d93d66
YeongWei <laiyw@persistent.my>
2016-01-07 03:08:12 -0500

    Sample test spec

---
+++ test/com/ibm/csi/npm/extension/DomainObjectSpec.scala
@@ -0,0 +1,10 @@
+package com.ibm.csi.npm.extension
+
+object DomainObjectSpec {
+  def main(args: Array[String]) = {
+    println("This is DomainObjectSpec!!!")
+  }
+}
+
+class DomainObjectSpec {
+}

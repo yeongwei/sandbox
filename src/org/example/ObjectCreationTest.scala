0ff69548e47bd512ae3af646908df6fed19fc4e6
YeongWei <laiyw@persistent.my>
2016-02-01 00:36:02 -0500

    Overloaded apply method

--- src/org/example/ObjectCreationTest.scala
+++ src/org/example/ObjectCreationTest.scala
@@ -4,6 +4,12 @@ import org.scalatest.FunSpec
 
 object ClassSetting {
   val x = 1
+  
+  def apply(): Unit = apply(0)
+  def apply(a: Int): Unit = apply(a, 1)
+  def apply(l: Long): Unit = apply(l.toInt, 1)
+  
+  def apply(a: Int, b: Int): Unit = {}
 }
 
 class Class(_x: Int = ClassSetting.x) {
@@ -12,9 +18,22 @@ class Class(_x: Int = ClassSetting.x) {
 
 class ObjectCreationTest extends FunSpec {
   describe("Initialize class with overloaded argument") {
-     it("should inherit value from object") {
+    it("should inherit value from object") {
        val c = new Class
        assert(c.x == ClassSetting.x)
      }
+  }
+  
+  describe("Inspect Scala Object overloading apply method") {
+    it("should not have exception") {
+      try {
+        ClassSetting(100)
+        assert(true)
+      } catch {
+        case ex: Throwable =>
+          ex.printStackTrace()
+          assert(false)
+      }
+    }
   }
 }

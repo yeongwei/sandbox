43acf313146116824f9a774bf05e5ef91deef351
YeongWei <laiyw@persistent.my>
2016-02-02 00:05:16 -0500

    Script to kill NPI frameworks

---
+++ script/killNpiFrameworks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+findPID() {
+	PIDs=(`ps -ef | grep ${1} | grep -v grep | awk '{print $2}'`)
+	echo ${PIDs[*]}
+}
+
+killCmd() {
+	PIDs=(`findPID "${1}"`)
+	echo "Search String: ${1}"
+	if [ ${#PIDs[@]} -eq 0 ] 
+	then
+		echo "No PIDs to kill"
+	fi
+	for pid in "${PIDs[@]}"
+	do
+		echo "PID to kill: ${pid}"
+		kill -9 ${pid}
+	done
+}
+
+FRAMEWORKS=("zookeeper" "kafka" "hadoop")
+
+for fw in "${FRAMEWORKS[@]}"
+do
+	killCmd "-i ${fw}"
+done

1c16f8cada4cf28b472b2c3809214913659675bb
YeongWei <laiyw@persistent.my>
2016-02-03 00:07:41 -0500

    Update killing sequence; Updated grep to avoid greddy grepping

--- script/killNpiFrameworks.sh
+++ script/killNpiFrameworks.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
 findPID() {
-	PIDs=(`ps -ef | grep ${1} | grep -v grep | awk '{print $2}'`)
+	PIDs=(`ps -ef | grep ${1} | grep -v grep | grep -v gedit | grep -v killNpiFrameworks | awk '{print $2}'`)
 	echo ${PIDs[*]}
+}
+
+checkKillStatus() {
+	killed=0
+	waitCount=10
+
+	while [ ${waitCount} -ne 0 ]; do
+		psResult=`ps -e | grep ${1}`
+		echo "[DEBUG] ${psResult}"
+		if [ -z "${psResult}" ]
+		then
+			killed=1
+			echo "PID ${1} has been killed"
+			break
+		fi
+		sleep 5
+		waitCount=$(( $waitCount - 1))
+	done
+
+	if [ ${killed} -eq 0 ]; then
+		echo "PID ${1} is still alive"
+	fi
 }
 
 killCmd() {
 	PIDs=(`findPID "${1}"`)
 	echo "Search String: ${1}"
+
 	if [ ${#PIDs[@]} -eq 0 ] 
 	then
 		echo "No PIDs to kill"
 	fi
+
 	for pid in "${PIDs[@]}"
 	do
 		echo "PID to kill: ${pid}"
-		kill -9 ${pid}
+		kill -15 ${pid}
+		checkKillStatus ${pid}
 	done
 }
 
-FRAMEWORKS=("zookeeper" "kafka" "hadoop" "npi")
+FRAMEWORKS=("npi" "hadoop" "kafka" "zookeeper")
 
 for fw in "${FRAMEWORKS[@]}"
 do
